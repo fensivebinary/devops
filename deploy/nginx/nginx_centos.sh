c140c64e4614777256401834105060b37f94a10a
Frozen Strawberries <18405214+frozenstrawberries@users.noreply.github.com>
2020-03-11 00:35:13 +0500

    added nginx-centos deploy script

---
+++ deploy/nginx/nginx_centos.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+
+echo "Installing Updates\n"
+yum -y install epel-release
+sed -i "s/mirrorlist=https/mirrorlist=http/" /etc/yum.repos.d/epel.repo
+echo "Done with the updates, Installing Nginx Now\n"
+yum -y install nginx
+echo "Done with the Installation, Starting Nginx Now\n"
+service nginx start
+
+# sed -i 's/ipaddress=""/ipaddress="1.1.1.1"/1' abc.txt
+
+echo "Done, the nginx is running on: "
+ip addr show eth0 | grep inet | awk '{ print $2; }' | sed 's/\/.*$//'
+echo
+echo "with port 80\n"
+
+echo "If you are running this from a VM then you need to configure iptables to access nginx from outside\n"
